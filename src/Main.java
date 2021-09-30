c8bb361e100de3c7850689864da230f26e47f13c
Paris Pettis <pettis_paris@hotmail.com>
2021-09-30 17:46:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    // OTP - One Time Pad
+    /*
+    * encryption technique that cannot be cracked,
+    * but requires the use of a one time pre-shared
+    * key the same size as, or longer than, the message being sent.
+    * In this technique, plaintext is paired with a random secret key
+    * (also referred to as a one-time pad)
+    */
+
+}
